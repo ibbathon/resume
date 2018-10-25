ec6e45a61545794d9486a0e50eee8ce0d9cfda2d
Ibb Marsh <ibbathon@gmail.com>
2018-10-24 22:12:48 -0500

    Switched to PostgreSQL
    Hooray for no longer having to drop/recreate the DB whenever I want to
    remove a column!

--- rails_resume/db/migrate/20181020185103_change_resume_section_item_relationship_part_3.rb
+++ rails_resume/db/migrate/20181020185103_change_resume_section_item_relationship_part_3.rb
@@ -1,5 +1,5 @@
 class ChangeResumeSectionItemRelationshipPart3 < ActiveRecord::Migration[5.2]
   def change
-    remove_reference :sections, :resume_id, foreign_key: true
+    remove_reference :sections, :resume, foreign_key: true
   end
 end
--- rails_resume/db/migrate/20181023233540_add_resume_layout_and_style_to_resume.rb
+++ rails_resume/db/migrate/20181023233540_add_resume_layout_and_style_to_resume.rb
@@ -1,9 +1,9 @@
 class AddResumeLayoutAndStyleToResume < ActiveRecord::Migration[5.2]
   def change
-    add_reference :resumes, :preferred_style, foreign_key: true
-    add_reference :resumes, :preferred_layout, foreign_key: true
-    add_reference :resumes, :preferred_pdf_style, foreign_key: true
-    add_reference :resumes, :preferred_pdf_layout, foreign_key: true
+    add_reference :resumes, :preferred_style, foreign_key: {to_table: :styles}
+    add_reference :resumes, :preferred_layout, foreign_key: {to_table: :resume_layouts}
+    add_reference :resumes, :preferred_pdf_style, foreign_key: {to_table: :styles}
+    add_reference :resumes, :preferred_pdf_layout, foreign_key: {to_table: :resume_layouts}
     add_column :resumes, :preferred_job, :string
   end
 end
